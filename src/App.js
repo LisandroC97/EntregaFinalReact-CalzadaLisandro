0fab0d733eb2ca60defff3f5f4c4fb6ced614c0e
Lisandro Calzada <clisandro1997@gmail.com>
2023-08-27 15:54:59 -0300

    main

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@
 import './App.css';
 import { ItemListContainer } from './components/ItemListContainer';
 import { NavBar } from './components/NavBar';
-import { catalog } from './components/Catalog';
+
 
 function App() {
   return (
     <>
     <NavBar/>
     <ItemListContainer greeting="Buenas!" />
-    <catalog />
     </>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
 import Container from "react-bootstrap/esm/Container";
-import products from '../data/products.json';
-console.log(products)
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import data from '../data/products.json';
 
-export const ItemListContainer=(props)=>{
-    return <div>{props.greeting}</div>
+export const ItemListContainer=(props)=>
+{
+    const [products, setProducts]=useState([]);
+    useEffect(()=>{
+        const promise = new Promise((resolve,reject)=>{setTimeout(()=>resolve(data),1500);
+        });
+        promise.then(data=>setProducts(data))
+        
+    },[]);
+    console.log(products)
+    return (<Container className="mt-4">
+        <h1>{props.greeting}</h1>
+        <div>
+            {products.map(product=>(
+                <Card key={product.id} style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={product.img} />
+                    <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>
+                        {product.material}
+                    </Card.Text>
+                    <Card.Text>
+                        {product.precio}
+                    </Card.Text>
+                    <Button variant="primary">Agregar al carrito
+                    </Button>
+                    </Card.Body>
+                </Card>
+            )
+            )}
+        </div>
+        </Container>
+    )
 }
